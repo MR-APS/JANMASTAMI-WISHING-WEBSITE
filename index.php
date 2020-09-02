4b6198647c6c34a9e7e971e644cb2bed696466dc
MR-APS <70637266+MR-APS@users.noreply.github.com>
2020-09-02 16:02:32 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,79 @@
+<html>
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport">
+<link rel="canonical" href="http://www.madhubanimix.com/"/>
+<link href='https://3.bp.blogspot.com/-N4q3NnAnwGw/W3KyhY3WaGI/AAAAAAAAB4E/ju6CQl1-A6Ei1YG6JieV3kh9zcngoI0rQCLcBGAs/s1600/rose2.png' rel='icon' type='image/x-icon'/> 
+<title>Create A Special Wishes. | By TechnoVedant</title>
+<meta name="description" content="Send Festival Wish to your loved one. | By TechnoVedant"/>
+<meta property="og:title" content="Create a special wishes. | By TechnoVedant"/>
+<meta property="og:site_name" content="Send Festival Wish to your loved one. | By TechnoVedant"/>
+<meta property="og:url" content="http://www.technovedant.com"/>
+<meta property="og:description" content="Send Festival Wish to your loved one. | By TechnoVedant"/>
+<meta property="og:type" content="article"/>
+<meta property="og:image" content="/img/ogg.png"/>
+<meta property="og:image:width" content="450"/>
+<meta property="og:image:height" content="298"/>
+<meta content='' property='article:author'/>
+<meta property="article:publisher" content="Akash Malik"/>
+<meta itemprop="name" content="Create a special wishes.">
+<meta itemprop="description" content="Send Festival Wish to your loved one. | By TechnoVedant"> 
+<meta itemprop="image" content="/img/logo.png">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/css/materialize.min.css">
+<link rel="stylesheet" href="/css/technovedant.css">
+
+</head>
+<body>
+<main>
+
+<div class="row container">
+<div class="col s12">
+<div class="valign-wrapper">
+<div class="col l3 hide-on-small-only">
+</div>
+<div class="col s12 l6 offset-l3">
+<div class="valign card-panel  z-depth-2" style="border: 1px solid rgba(255, 255, 255, 0.1);border-radius: 5px;">
+<div class="card-content">
+<div class="row">
+<div class="create-idea">
+ 
+<img class="center" src="/img/logo.png" height="88px" width="270px" alt="TechnoVedant"><br>
+<section class="cd-intro">
+<h5 class="cd-headline letters type">
+<span>Create Your Wishes For Janmashtami</span>
+</h5>
+</section>
+</div>
+<form method="get" action="krishna.php">
+
+<div class="input-field col s12 ">
+<input id="username" onkeydown="validateAlpha();" type="text" class="validate" required="" maxlength="25" name="n" style="    font-size: 16px;" placeholder="&#128073; Enter Your Name In English" pattern="[a-zA-Z][a-zA-Z0-9-_ .]{1,25}" title="Enter Your Name">
+<label for="username" style="color:#FFFFFF;font-size: 14px;"></label>
+</div>
+
+</div>
+
+<p id="error-display" style="display:none;"></p>
+<button class="btn waves-effect waves-light" onclick="create();" type="submit" style="width:100%; margin-top: 10px;">Click Here</button>
+
+</div>
+</div>
+</div>
+</div>
+</div>
+</form>
+</div>
+ 
+</div>
+</main>
+<footer class="page-footer blue-grey darken-1">
+<div class="footer-copyright">
+<div class="container">
+<a class="grey-text text-lighten-4" href="#" target="_blank">Technovedant</a>
+<a class="grey-text text-lighten-4 right" href="" target="_blank">Happy Janmashtami</a>
+</div>
+</div>
+</footer>
+</body>
+</html>
